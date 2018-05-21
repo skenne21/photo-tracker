2346688265b87b951219c5384df69c22984c8ccc
Sabrina Kennedy <skenne21@gmail.com>
2018-05-21 11:59:30 -0600

    Adding photoswhen button is clicked

--- public/script.js
+++ public/script.js
@@ -25,7 +25,7 @@ const setPhoto = async (photo) => {
       body: JSON.stringify(photo),
       headers: { 'Content-Type': 'application/json' }
     });
-    fetchPhotos();
+    await fetchPhotos();
   } catch ( error ) {
     console.log(error);
   }
@@ -34,16 +34,27 @@ const setPhoto = async (photo) => {
 const fetchPhotos = async () => {
   try {
     const response = await fetch('/api/v1/photos');
-    const photos = response.json();
-    appendPhotos(photos);
+    const photos = await response.json();
+    await appendPhotos(photos);
   } catch (error) {
     console.log(error)
   }
 }
 
 const appendPhotos = (photos) => {
-  const parnet = $('.show_photos');
-  
+  console.log(photos)
+  $('.show_photos').empty();
+  console.log(photos)
+  const createdPhotos = photos.map(photo => {
+    return (`
+      <article>
+        <h3>Title:${photo.title}</h3>
+        <img src="${photo.url}"/>
+        <button class="delete">X</button>
+      </article>
+    `)
+  })
+  $('.show_photos').append(createdPhotos)
 }
 
 $('.add_photos').on('click', createPhotos);
